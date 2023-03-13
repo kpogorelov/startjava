286a1c78c12b5a4d9aa70672f77f6f496ee21c99
konstantin <pogorelovkonstantin@mail.ru>
2023-03-13 19:23:09 +0400

    Impl Calculator

---
+++ Lesson_1/src/main/java/org/example/Calculator.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int firstDigit = 8;
+        int secondDigit = 5;
+        char operationChar = '+';
+        if (operationChar == '+' || operationChar == '-' || operationChar == '*' || operationChar == '/'
+                || operationChar == '^' || operationChar == '%') {
+            if (operationChar == '+') {
+                System.out.println(firstDigit + " " + operationChar + " " + secondDigit + " = " + (firstDigit + secondDigit));
+            } else if (operationChar == '-') {
+                System.out.println(firstDigit + " " + operationChar + " " + secondDigit + " = " + (firstDigit - secondDigit));
+            } else if (operationChar == '*') {
+                System.out.println(firstDigit + " " + operationChar + " " + secondDigit + " = " + (firstDigit * secondDigit));
+            } else if (operationChar == '/') {
+                System.out.println(firstDigit + " " + operationChar + " " + secondDigit + " = " + (firstDigit / secondDigit));
+            } else if (operationChar == '%') {
+                System.out.println(firstDigit + " " + operationChar + " " + secondDigit + " = " + (firstDigit % secondDigit));
+            } else {
+                System.out.print(firstDigit + " " + operationChar + " " + secondDigit + " = ");
+                int copyFirstDigit = firstDigit;
+                while (secondDigit - 1 > 0) {
+                    firstDigit *= copyFirstDigit;
+                    secondDigit--;
+                }
+                System.out.println(firstDigit);
+            }
+        } else {
+            System.out.println("Неизвестный оператор поля operationChar");
+        }
+    }
+}
