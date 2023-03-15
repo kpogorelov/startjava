09c68ac7f3a58b60ea448d5a27e14f11e13a8f43
konstantin <pogorelovkonstantin@mail.ru>
2023-03-15 18:26:43 +0400

    Patch2 Final h_w

--- Lesson_1/src/main/java/org/example/Calculator.java
+++ Lesson_1/src/main/java/org/example/Calculator.java
@@ -4,8 +4,9 @@ public class Calculator {
     public static void main(String[] args) {
         int number1 = 2;
         int number2 = 3;
-        char mathSign = '^';
+        char mathSign = '*';
         int result = 0;
+        boolean correctChar = true;
         if (mathSign == '+') {
             result = number1 + number2;
         } else if (mathSign == '-') {
@@ -23,8 +24,11 @@ public class Calculator {
             }
         } else {
             System.out.println(mathSign + " - такая операция не поддерживается");
+            correctChar = false;
         }
-        System.out.println(number1 + " " + mathSign + " " + number2 + " = " +
-                "" + result);
+        if (correctChar) {
+            System.out.println(number1 + " " + mathSign + " " + number2 + " = " +
+                    "" + result);
+        }
     }
 }
